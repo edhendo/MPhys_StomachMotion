c5828b81f54490944c0cfa30eff53964f1154d19
eleanorscott97 <47561593+eleanorscott97@users.noreply.github.com>
2019-03-07 17:01:31 +0000

    Update stomach_isosurface2.py

--- stomach_isosurface2.py
+++ stomach_isosurface2.py
@@ -19,31 +19,57 @@ pca_result_cube = np.load('C:\MPhys\\Data\\Intra Patient\\Pancreas\\PCA\\niftyre
 
 # Read in the delineation nifti files using nibabel
 #stomach = nib.load('C:\MPhys\\Nifti_Images\\niftyregPanc01StomachCrop\\stomachMask.nii');
-stomach = nib.load('C:\MPhys\Data\Intra Patient\Pancreas\niftyregPanc01StomachCrop\\stomachMask.nii')
+stomach = nib.load('C:\MPhys\\Data\\Intra Patient\\Pancreas\\niftyregPanc01StomachCrop\\stomach.nii')
 stomachHdr = stomach.header;
 stomachData = stomach.get_fdata();
-stomach_PRV = nib.load('C:\MPhys\Data\Intra Patient\Pancreas\niftyregPanc01StomachCrop\\stomach_PRVMask.nii');
+'''
+stomach_PRV = nib.load('C:\MPhys\\Data\\Intra Patient\\Pancreas\\niftyregPanc01StomachCrop\\stomach_PRVMask.nii');
 stomach_PRVHdr = stomach_PRV.header;
 stomach_PRVData = stomach_PRV.get_fdata();
-
+'''
 # numpy array conversion
 stom = np.array(stomachData);
-stomPRV= np.array(stomach_PRVData);
+#stomPRV= np.array(stomach_PRVData);
 
 # Use marching cubes to obtain the surface mesh of the stomach/stomach PRV delineations
 # input 3d volume - masking data form WM
-verts, faces, normals, values = measure.marching_cubes_lewiner(, 50)
+verts, faces, normals, values = measure.marching_cubes_lewiner(stom, 50)
+
+#------------------ save vertex and face coordinates into txt files --------------------------------------------------------------
+np.savetxt('C:\MPhys\\Data\\Intra Patient\\Pancreas\\3D Vis\\stomachVerts01.txt',verts, fmt = '%0.6f')
+# create new array for faces - needs to have 4 components (-1 as fourth)
+facesnew = np.ndarray(shape = (faces.shape[0],4))
+for i in range(faces.shape[0]):
+    for j in range(3):
+        facesnew[i][j] = int(faces[i][j])
+    facesnew[i][3] = int(-1)
+    
+np.savetxt('C:\MPhys\\Data\\Intra Patient\\Pancreas\\3D Vis\\stomachFaces01.txt',facesnew.astype(int), fmt = '%0.0f')
+#--------------------------------------------------------------------------------------------------------------------------------
 
 # Display resulting triangular mesh
-fig = plt.figure()
+fig = plt.figure(figsize=(10,10))
 ax = fig.add_subplot(111, projection='3d')
 
 # Fancy indexing: `verts[faces]` to generate a collection of triangles
 mesh = Poly3DCollection(verts[faces])
 mesh.set_edgecolor('k')
 ax.add_collection3d(mesh)
-=======
-# Use marching cubes to obtain the surface mesh of the stomach delineation
+
+#set axis labels
+ax.set_xlabel("x-axis: L-R?")
+ax.set_ylabel("y-axis: A-P?")
+ax.set_zlabel("z-axis: C-C?")
+
+#plot axis limits based on mesh dimensions
+ax.set_xlim(verts[:,0].min() - 2, verts[:,0].max() + 2)
+ax.set_ylim(verts[:,1].min() - 2, verts[:,1].max() + 2)  
+ax.set_zlim(verts[:,2].min() - 2, verts[:,2].max() + 2)  
+
+plt.tight_layout()
+plt.show()
+
+
 
 
 

0bbc9c371def2eec77f812a3b775026c6a0b2479
Edward Henderson <edward.henderson@student.manchester.ac.uk>
2019-03-07 14:59:25 +0000

    Create stomach_isosurface.py

---
+++ stomach_isosurface.py
@@ -0,0 +1,25 @@
+# stomach_isosurface.py
+"""
+Created on Thu Mar  7 14:58:10 2019
+
+@author: Edward Henderson
+"""
+
+import time
+import numpy as np
+import nibabel as nib
+from skimage import feature
+
+t1 = time.time();
+np.set_printoptions(precision=4, suppress=True);
+pca_result_cube = np.load('C:\MPhys\\Data\\PCA results\\niftyregPanc01StomachCropPCAcube.npy');
+# Read in the delineation nifti files using nibabel
+stomach = nib.load('C:\MPhys\\Nifti_Images\\niftyregPanc01StomachCrop\\stomachMask.nii');
+stomachHdr = stomach.header;
+stomachData = stomach.get_fdata();
+stomach_PRV = nib.load('C:\MPhys\\Nifti_Images\\niftyregPanc01StomachCrop\\stomach_PRVMask.nii');
+stomach_PRVHdr = stomach_PRV.header;
+stomach_PRVData = stomach_PRV.get_fdata();
+# numpy array conversion
+stom = np.array(stomachData);
+stomPRV= np.array(stomach_PRVData);

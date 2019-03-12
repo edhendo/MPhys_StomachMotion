4a55bfb1344af0ec495e2f39363181e8ad110b83
eleanorscott97 <47561593+eleanorscott97@users.noreply.github.com>
2019-03-12 15:02:37 +0000

    Update stomach_isosurface2.py

--- stomach_isosurface2.py
+++ stomach_isosurface2.py
@@ -83,4 +83,3 @@ for x in range(verts.shape[0]):
 #write into a text file
 np.savetxt('C:\MPhys\\Data\\Intra Patient\\Pancreas\\3D Vis\\stomachColours01.txt', colours, fmt = '%0.6f')
 
-#issue with scaling for PCA - addition of multiple components results in RGB values > 1

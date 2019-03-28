5f7e156ab682ed17ea2adc5d90b2b7a4e6ad3959
eleanorscott97 <47561593+eleanorscott97@users.noreply.github.com>
2019-03-28 18:44:34 +0000

    changes

--- Ploty_isosurface.py
+++ Ploty_isosurface.py
@@ -5,6 +5,9 @@ Created on Thu Mar 21 15:25:28 2019
 @author: Eleanor
 """
 import plotly
+plotly.tools.set_credentials_file(username='eleanor1357', api_key='oB200N0KGmaub83XuktV')
+plotly.tools.set_config_file(world_readable=False,
+                             sharing='private')
 import plotly.plotly as py
 import plotly.figure_factory as ff
 import plotly.graph_objs as go
@@ -14,34 +17,47 @@ import time
 import nibabel as nib
 from sklearn.preprocessing import MinMaxScaler
 from matplotlib import cm
+ 
+
 
 
 tStart = time.time();
 np.set_printoptions(precision=4, suppress=True);
+############################## load data ########################
 #mag_pca_result_cube = np.load('C:\MPhys\\Data\\PCA results\\Panc01StomachCropMagnitudePCAcube.npy');
-mag_pca_result_cube = np.load('C:\MPhys\\Data\\Intra Patient\\Stomach\\PCA\\pcaMagPanc01.npy');
+mag_pca_result_cube = np.load('C:\MPhys\\Data\\Intra Patient\\Stomach\\PCA\\pcaMagStomach04.npy');
 
-toggle = True; # set to True for using pca on magnitudes rather than magnitude of pca comps
 
 # Read in the delineation nifti files using nibabel
 #stomach = nib.load('C:\MPhys\\stomach.nii');
-stomach = nib.load('C:\MPhys\\Data\\Intra Patient\\Stomach\\Panc01_NR\\stomach.nii')
+stomach = nib.load('C:\MPhys\\Data\\Intra Patient\\Stomach\\Stomach04\\stomachMask.nii')
 stomachHdr = stomach.header;
 stomachData = stomach.get_fdata();
 
 # numpy array conversion
-#stom = np.rot90(np.rot90(np.array(stomachData),2,(0,2)),1,(1,2));
+# stom = np.rot90(np.rot90(np.array(stomachData),2,(0,2)),1,(1,2));
 stom = np.array(stomachData);
+
+##################### functions ###############################
+
+def tri_indices(faces):
+    #simplices is a numpy array defining the simplices of the triangularization
+    #returns the lists of indices i, j, k
+
+    return ([triplet[c] for triplet in faces] for c in range(3))
+
+################ create trisurf plot #########################
 
 # Use marching cubes to obtain the surface mesh of the stomach/stomach PRV delineations
 # input 3d volume - masking data form WM
-verts, faces, normals, values = measure.marching_cubes_lewiner(stom, 50) #note to self:check masking boudaries in lua code 
-x,y,z = zip(*verts)
-#i,j,k = zip(*faces)
+verts, faces, normals, values = measure.marching_cubes_lewiner(stom, 50)
+X3,Y3,Z3 = zip(*verts)
+X3 = np.array(X3)
+Y3 = np.array(Y3)
+Z3 = np.array(Z3)
+i,j,k = tri_indices(faces)
 
-
-#find the PCA vector values that correspond with mesh vertices
-#put the PCA values that match the rounded vertex values into an array
+# find the PCA vector values that correspond with mesh vertices and put the values that match the rounded vertex values into an array
 colours = np.ndarray(shape = (verts.shape[0],3))
 coloursMag = np.ndarray(shape = (verts.shape[0]))
 #round vertex numbers to nearest int
@@ -58,14 +74,20 @@ colourmap = cm.YlOrRd(coloursMag);
 for X in range(verts.shape[0]):
     for l in range(3):
         colours[X,l] = colourmap[X,0,l];
-
-#ff.create_trisurf
-data = go.Mesh3d(x,y,z, faces, colormap='RdBu', plot_edges=None, vertexcolor = colours)
-
-title="Panc01 - PCA Magnitudes"
+        
+#function to assign colours to vertices
+def vertexColour(x,y,z):
+    for h in range(X3.size):
+        if X3[h] == x:
+            Index = h
+    return colours(Index)
+        
+        
+data = ff.create_trisurf(x=X3, y=Y3, z=Z3, simplices = faces, color_func = vertexColour, show_colorbar = True)
+#data = [go.Mesh3d(x=x,y=y,z=z, i=i,j=j,k=k, vertexcolor = colours, colorscale = 'YlOrRd', colorbar = dict(thickness = 20, x = 2))]
 
 fig3 = go.Figure(data=data)
-fig3['layout'].update(dict(title=title,
+fig3['layout'].update(dict(title= ' Stomach04 - PCA Magnitudes',
                            width=1000,
                            height=1000,
                            scene=dict(
@@ -75,4 +97,4 @@ fig3['layout'].update(dict(title=title,
                            )
                      ))
 
-plotly.offline.plot(fig3, filename = 'Panc01 - PCA Magnitudes')
+plotly.offline.plot(fig3, filename = 'Stomach04 - PCA Magnitudes.html')
--- Variance Graph Plot.py
+++ Variance Graph Plot.py
@@ -21,7 +21,7 @@ stomach07 = np.load('C:\MPhys\\Data\\Intra Patient\\Stomach\\PCA\\EVR_Stomach07.
 
 # x component
 pcomponents = np.linspace(1,9,9)
-
+pcomponents2 = np.linspace(2,9,8)
 ###############################################################################################################
 plt.figure(1)
 plt.plot(pcomponents, panc01, 'o-', markersize = 5, clip_on = False, zorder = 10, label = 'Patient 01')
@@ -60,3 +60,22 @@ plt.yticks(fontsize = 14)
 plt.grid(True)
 plt.legend()
 plt.savefig('C:\MPhys\\Data\\Intra Patient\\Stomach\\PCA Graphs and Images\\All_Patients_LogVariance.png')
+
+plt.figure(3)
+plt.plot(pcomponents2, panc01[1:], 'o-', markersize = 5, clip_on = False, zorder = 10, label = 'Patient 01')
+plt.plot(pcomponents2, stomach02[1:], 'o-', markersize = 5, clip_on = False, zorder = 10, label = 'Patient 02')
+plt.plot(pcomponents2, stomach04[1:], 'o-', markersize = 5, clip_on = False, zorder = 10, label = 'Patient 04')
+plt.plot(pcomponents2, stomach05[1:], 'o-', markersize = 5, clip_on = False, zorder = 10, label = 'Patient 05')
+plt.plot(pcomponents2, stomach06[1:], 'o-', markersize = 5, clip_on = False, zorder = 10, label = 'Patient 06')
+plt.plot(pcomponents2, stomach07[1:], 'o-', markersize = 5, clip_on = False, zorder = 10, label = 'Patient 07')
+
+plt.title('Percentage Variance for each Principle Component',fontsize = 16)
+plt.xlabel('Principal Component', fontsize = 16)
+plt.ylabel('Percentage of total variance', fontsize = 16)
+plt.ylim(0,0.12)
+plt.xlim(1,9)
+plt.xticks(fontsize = 14)
+plt.yticks(fontsize = 14)
+plt.grid(True)
+plt.legend()
+plt.savefig('C:\MPhys\\Data\\Intra Patient\\Stomach\\PCA Graphs and Images\\All_Patients_Variance2.png')

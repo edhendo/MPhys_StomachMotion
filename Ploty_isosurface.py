453cc76af2353441e8b6aa0a0618ffd6cab02993
eleanorscott97 <47561593+eleanorscott97@users.noreply.github.com>
2019-03-29 01:17:38 +0000

    updates

--- Ploty_isosurface.py
+++ Ploty_isosurface.py
@@ -4,6 +4,9 @@ Created on Thu Mar 21 15:25:28 2019
 @author: Eleanor
 """
 import plotly
+plotly.tools.set_credentials_file(username='eleanor1357', api_key='oB200N0KGmaub83XuktV')
+plotly.tools.set_config_file(world_readable=True,
+                             sharing='public')
 import plotly.plotly as py
 import plotly.figure_factory as ff
 import plotly.graph_objs as go
@@ -13,34 +16,47 @@ import time
 import nibabel as nib
 from sklearn.preprocessing import MinMaxScaler
 from matplotlib import cm
+ 
+
 
 
 tStart = time.time();
 np.set_printoptions(precision=4, suppress=True);
+############################## load data ########################
 #mag_pca_result_cube = np.load('C:\MPhys\\Data\\PCA results\\Panc01StomachCropMagnitudePCAcube.npy');
-mag_pca_result_cube = np.load('C:\MPhys\\Data\\Intra Patient\\Stomach\\PCA\\pcaMagPanc01.npy');
+mag_pca_result_cube = np.load('C:\MPhys\\Data\\Intra Patient\\Stomach\\PCA\\pcaMagStomach04.npy');
 
-toggle = True; # set to True for using pca on magnitudes rather than magnitude of pca comps
 
 # Read in the delineation nifti files using nibabel
 #stomach = nib.load('C:\MPhys\\stomach.nii');
-stomach = nib.load('C:\MPhys\\Data\\Intra Patient\\Stomach\\Panc01_NR\\stomach.nii')
+stomach = nib.load('C:\MPhys\\Data\\Intra Patient\\Stomach\\Stomach04\\stomachMask.nii')
 stomachHdr = stomach.header;
 stomachData = stomach.get_fdata();
 
 # numpy array conversion
-#stom = np.rot90(np.rot90(np.array(stomachData),2,(0,2)),1,(1,2));
+# stom = np.rot90(np.rot90(np.array(stomachData),2,(0,2)),1,(1,2));
 stom = np.array(stomachData);
+
+##################### functions ###############################
+
+def tri_indices(faces):
+    #simplices is a numpy array defining the simplices of the triangularization
+    #returns the lists of indices i, j, k
+
+    return ([triplet[c] for triplet in faces] for c in range(3))
+
+################ create trisurf plot #########################
 
 # Use marching cubes to obtain the surface mesh of the stomach/stomach PRV delineations
 # input 3d volume - masking data form WM
-verts, faces, normals, values = measure.marching_cubes_lewiner(stom, 50) #note to self:check masking boudaries in lua code 
+verts, faces, normals, values = measure.marching_cubes_lewiner(stom, 50)
 x,y,z = zip(*verts)
-#i,j,k = zip(*faces)
+x = np.array(x)
+y = np.array(y)
+z = np.array(z)
+i,j,k = tri_indices(faces)
 
-
-#find the PCA vector values that correspond with mesh vertices
-#put the PCA values that match the rounded vertex values into an array
+# find the PCA vector values that correspond with mesh vertices and put the values that match the rounded vertex values into an array
 colours = np.ndarray(shape = (verts.shape[0],3))
 coloursMag = np.ndarray(shape = (verts.shape[0]))
 #round vertex numbers to nearest int
@@ -58,20 +74,37 @@ for X in range(verts.shape[0]):
     for l in range(3):
         colours[X,l] = colourmap[X,0,l];
 
-#ff.create_trisurf
-data = go.Mesh3d(x,y,z, faces, colormap='RdBu', plot_edges=None, vertexcolor = colours)
+color_trace = go.Scatter(x=[0 for _ in colours],
+                            y=[0 for _ in colours],
+                            mode='markers',
+                            marker= dict(
+                                colorscale= 'YlOrRd',
+                                reversescale = True,
+                                size=1,
+                                colorbar = dict(len = 0.5),
+                                color=colours,
+                                showscale=True,
+                                )
+                            )    
 
-title="Panc01 - PCA Magnitudes"
+data = [go.Mesh3d(x=x,y=y,z=z, i=i,j=j,k=k, vertexcolor = colours), color_trace]
 
 fig3 = go.Figure(data=data)
-fig3['layout'].update(dict(title=title,
-                           width=1000,
-                           height=1000,
-                           scene=dict(
-                                      aspectratio=dict(x=1, y=1, z=0.4),
-                                      camera=dict(eye=dict(x=1.25, y=1.25, z= 1.25)
-                                     )
+fig3['layout'].update(dict(title= ' Stomach04 - PCA Magnitudes',
+                            xaxis = dict(type = 'linear', showticklabels = False, showgrid = False,
+                                        zeroline = False,
+                                        autorange = True),
+                            yaxis = dict(type = 'linear', showticklabels = False, showgrid = False,
+                                        zeroline = False,
+                                        autorange = True),
+                            width = 1000,
+                            height = 1000,
+                            scene = dict(xaxis = dict(type = 'linear', showticklabels = False),
+                                        yaxis = dict(type = 'linear', showticklabels = False),
+                                        zaxis = dict(type = 'linear', showticklabels = False),
+                                        aspectratio=dict(x=1, y=1, z=0.75),
+                                        camera=dict(eye=dict(x=1.25, y=1.25, z= 1.25)
+                                        )
                            )
                      ))
-
-plotly.offline.plot(fig3, filename = 'Panc01 - PCA Magnitudes')
+py.plot(fig3, filename = 'Stomach04 - PCA Magnitudes.html')
